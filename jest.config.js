3a520903d5bd395bcb61463778643aadf6d53349
himself65 <himself6565@gmail.com>
2019-01-22 00:52:47 +0800

    [add] babel
    [add] jest
    
    [fix] lint

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json'
+  ],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testURL: 'http://localhost/'
+}
