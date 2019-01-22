a8b67ffaedd13422ce15ee5e74012ab9967f0fa9
himself65 <himself6565@gmail.com>
2019-01-22 13:32:37 +0800

    using material design with @material-ui
    [update] AppBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-import './App.css'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/es/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import './App.styl'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      <div className="app">
+        <AppBar>
+          <Toolbar position="static">
+            <Typography variant="h6" color="inherit">
+              点名器
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
